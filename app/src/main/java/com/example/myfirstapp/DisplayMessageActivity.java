4e2f41b14a8de9a5e18f7ac863fbbea9b4853e47
Pellier <pacha.em@gmail.com>
2020-05-06 18:55:25 +0900

    testttt

--- app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
@@ -3,10 +3,12 @@ package com.example.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com. example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +21,13 @@ public class DisplayMessageActivity extends AppCompatActivity {
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView);
         textView.setText(message);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, Main3Activity.class);
+        //EditText editText = (EditText) findViewById(R.id.textView);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/myfirstapp/Main3Activity.java
@@ -0,0 +1,22 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        Intent intent = getIntent();
+        //String message = intent.getStringExtra(DisplayMessageActivity.EXTRA_MESSAGE);
+
+        TextView textView = findViewById(R.id.textView3);
+        textView.setText("oaiefeifeife");
+    }
+}
